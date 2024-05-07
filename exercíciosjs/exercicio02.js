c3cbd3b647ee8f922901fd3f438c95d3285ed9d6
Márcio Davidson <166762462+marciodavidson@users.noreply.github.com>
2024-05-07 16:42:12 -0300

    Testendo Div e mouse
    aprendi a mexer mais com as funções do mouse

---
+++ exercíciosjs/exercicio02.js
@@ -0,0 +1,8 @@
+//EAI GALERA BELEZA? SOU MÁRCIO DAVIDSON E ESTOU APRENDENDO A PROGRAMAR EM JAVASCRIPT. COMEÇAREI COM A PRIMEIRA FUNÇÃO QUE EU APRENDI
+
+let nome = window.prompt('Qual é o seu nome?')
+let res = window.document.getElementById ('Resultado')
+
+res.innerHTML = `p Olá seja bem-vindo ${nome}! É um enorme prazer te conhecer!`
+
+
